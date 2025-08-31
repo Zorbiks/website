5911e91273941618ee71fcea0297cc591daceae3
Zorbiks <176812563+Zorbiks@users.noreply.github.com>
2025-09-01 00:50:03 +0100

    Add flashbang

--- js/theme.js
+++ js/theme.js
@@ -1,21 +1,44 @@
+const flashbang = document.getElementById("flashbang");
 const themeToggle = document.getElementById("toggle");
+const themeToggleBackground = document.querySelector(".toggle-label");
 
-const storedTheme = localStorage.getItem("theme");
+const storedTheme = localStorage.getItem("theme") || document.documentElement.dataset.theme;
 
-if (storedTheme) {
-    document.documentElement.dataset.theme = storedTheme;
-    if (storedTheme == "dark") {
-        themeToggle.setAttribute("checked", "checked");
-    }
+// Turn on/off theme toggle switch based on the stored theme
+if (storedTheme == "dark") {
+    themeToggle.setAttribute("checked", "checked");
 }
 
-themeToggle.addEventListener("click", event => {
+// Toggle between light/dark theme and trigger flashbang with sound effect
+themeToggle.addEventListener("click", () => {
     const theme = document.documentElement.dataset.theme;
+
     if (theme === "light") {
-        document.documentElement.dataset.theme = "dark"
-        localStorage.setItem("theme", "dark")
+        animateThemeToggle()
+        document.documentElement.dataset.theme = "dark";
+        localStorage.setItem("theme", "dark");
+        flashbang.classList.remove("active");
     } else {
-        document.documentElement.dataset.theme = "light"
-        localStorage.setItem("theme", "light")
+        animateThemeToggle()
+        playAudio();
+        flashbang.classList.add("fadeout");
+        flashbang.classList.add("active");
+        document.documentElement.dataset.theme = "light";
+        localStorage.setItem("theme", "light");
     }
 });
+
+// Create Audio object and load audio file
+const audio = new Audio("/audio/flashbang.ogg");
+
+function playAudio() {
+    audio.play().catch((error) => {
+        console.error("Error playing audio:", error);
+        alert("Error playing audio file");
+    });
+}
+
+function animateThemeToggle() {
+    themeToggle.classList.add("animate");
+    themeToggleBackground.classList.add("animate");
+}
